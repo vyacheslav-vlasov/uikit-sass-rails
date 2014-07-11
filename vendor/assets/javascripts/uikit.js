d0dc6f9e9eab6f5d230281be86290633f6ca7812
Christian Worreschk <christianworreschk@Christian-CW-0002.local>
2014-07-11 10:50:29 +0200

    UIkit 2.8.0

---
+++ vendor/assets/javascripts/uikit.js
@@ -0,0 +1,14 @@
+//= require uikit/uikit
+
+//= require uikit/addons/datepicker
+//= require uikit/addons/form-password
+//= require uikit/addons/form-select
+//= require uikit/addons/htmleditor
+//= require uikit/addons/nestable
+//= require uikit/addons/notify
+//= require uikit/addons/pagination
+//= require uikit/addons/search
+//= require uikit/addons/sortable
+//= require uikit/addons/sticky
+//= require uikit/addons/timepicker
+//= require uikit/addons/upload
