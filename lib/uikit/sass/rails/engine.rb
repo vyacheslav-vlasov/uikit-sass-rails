3de131d0773ea2f531761d563f7ae31079ce90ce
Christian Worreschk <christianworreschk@Christian-CW-0002.local>
2014-07-11 10:50:57 +0200

    Font Awesome

--- lib/uikit/sass/rails/engine.rb
+++ lib/uikit/sass/rails/engine.rb
@@ -3,6 +3,9 @@ module Uikit
     module Rails
       class Engine < ::Rails::Engine
         isolate_namespace Uikit::Sass::Rails
+        initializer 'uikitkit-sass-rails.assets.precompile' do |app|
+          app.config.assets.precompile += %w(fontawesome-webfont.eot fontawesome-webfont.svg fontawesome-webfont.ttf fontawesome-webfont.woff)
+        end
       end
     end
   end
