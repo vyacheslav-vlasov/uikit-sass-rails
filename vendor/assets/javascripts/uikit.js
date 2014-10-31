1ba8bdece7ec58e5fc9d68ad1cc9e9340131c749
Christian Worreschk <cw@marsec.de>
2014-10-31 10:41:41 +0100

    UIKit 2.11.1 JavaScript

--- vendor/assets/javascripts/uikit.js
+++ vendor/assets/javascripts/uikit.js
@@ -1,16 +1,2 @@
 //= require uikit/uikit
-
-//= require uikit/addons/autocomplete
-//= require uikit/addons/cover
-//= require uikit/addons/datepicker
-//= require uikit/addons/form-password
-//= require uikit/addons/form-select
-//= require uikit/addons/htmleditor
-//= require uikit/addons/nestable
-//= require uikit/addons/notify
-//= require uikit/addons/pagination
-//= require uikit/addons/search
-//= require uikit/addons/sortable
-//= require uikit/addons/sticky
-//= require uikit/addons/timepicker
-//= require uikit/addons/upload
+//= require_tree uikit/components
